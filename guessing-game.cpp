dae4d36f49da766c4a65609e1648ce66c33f733c
Junaid Dawood <1094837@students.wwits.ac.za>
2018-07-21 09:56:52 +0200

    Exercise 2.1

---
+++ guessing-game.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+int main()
+{
+	//declaring round limit
+	const int roundLimit=5;
+
+	std::cout<<"Guess the random number"<<std::endl;
+	//random seed, target set
+	srand(time(0));
+	int targetNo = rand()%100 +1;
+
+	for (int i=0;i<roundLimit;i++)
+	{
+		
+		int userGuess=0;
+		std::cin>>userGuess;
+		//checking for good input
+		while (!std::cin.good())
+		{
+			std::cout<<"Not an Int, please enter again"<<std::endl;
+			std::cin.clear();
+			std::cin.ignore(INT8_MAX, '\n');
+			std::cin>>userGuess;
+
+		}
+		//appropriate output messages
+		if(userGuess>targetNo)
+		{
+			std::cout<<"Guess Lower"<<std::endl;
+		}else if(userGuess<targetNo)
+		{
+			std::cout<<"Guess Higher"<<std::endl;
+		}else
+		{	//win-exit application
+			std::cout<<"You Win"<<std::endl;
+			return 0;
+		}
+	}
+	std::cout<<"You Lose"<<std::endl;
+	std::cout<<"The number is "<<targetNo<<std::endl;
+	return 0;
+}
